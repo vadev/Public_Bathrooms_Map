0ae31412dfa4cadef51342a0a9552a270748ac51
Vartan Arzumanyan <bigballer131@gmail.com>
2025-08-26 08:03:57 -0700

    added vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,35 @@
+// import localFont from "next/font/local";
+// import "./globals.css";
+
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
+
+// export const metadata = {
+//   title: "Los Angeles Public Library Public Hydration Stations",
+//   description: "Los Angeles Public Library Public Hydration Stations",
+// };
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body
+//         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+//       >
+//         {children}
+//       </body>
+//     </html>
+//   );
+// }
+
+import { Analytics } from "@vercel/analytics/react"; 
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -24,6 +56,8 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        {/* Vercel Analytics */}
+        <Analytics />
       </body>
     </html>
   );
